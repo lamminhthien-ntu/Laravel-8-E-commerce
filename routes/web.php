bf545e57ef07aede93437aa3ce89f069e52ff740
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-11-09 07:31:34 +0700

    Laravel 8 Namespace controller

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth','isAdmin')->group(function () {
     Route::get('/dashboard', function () {
